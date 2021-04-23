6a20acf31b57a6b223e2d7d34729514101fa8786
Co9xs <ryo.fjsm0203@icloud.com>
2021-04-23 11:14:43 +0900

    リアルDOMを生成する関数を作成

--- src/framework/view.ts
+++ src/framework/view.ts
@@ -1,9 +1,11 @@
+// Nodeが取りうる3種の型
 type NodeType = VNode | string | number
+// 属性の型
 type Attributes = {
   [key: string]: string | Function
 }
 
-// 仮想DOM
+// 仮想DOMの一つのオブジェクトの型
 export interface VNode {
   nodeName: keyof HTMLElementTagNameMap;
   attributes: Attributes;
@@ -17,4 +19,37 @@ export function h(
   ...children: NodeType[]
 ): VNode {
   return { nodeName, attributes, children }
+}
+
+// 仮想DOMからリアルDOMを生成する関数
+export function createElement(node: NodeType): HTMLElement | Text {
+  if (!isVNode(node)) {
+    return document.createTextNode(node.toString())
+  }
+  const element = document.createElement(node.nodeName)
+  setAttributes(element, node.attributes);
+  node.children.forEach(child => element.appendChild(createElement(child)))
+  return element
+}
+
+// onClickなどのイベントのときはイベントリスナを登録、それ以外は属性として登録する関数
+function setAttributes(target: HTMLElement, attrs: Attributes) {
+  for (let attr in attrs) {
+    if (isEventAttr(attr)) {
+      const eventName = attr.slice(2);
+      target.addEventListener(eventName, attrs[attr] as EventListener)
+    } else {
+      target.setAttribute(attr, attrs[attr] as string)
+    }
+  }
+}
+
+// Nodeを受け取り仮想DOMかどうかを判定
+function isVNode(node: NodeType): node is VNode {
+  return typeof node !== "string" && typeof node !== "number"
+}
+
+//属性を受け取りイベントかどうかを判定
+function isEventAttr(attr: string): boolean {
+  return /^on/.test(attr);
 }
