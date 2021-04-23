36096d0ab1b7e260a3e6d5bc787620840d317e27
Co9xs <ryo.fjsm0203@icloud.com>
2021-04-23 13:39:52 +0900

    サンプルアプリを実装

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-const log = (arg: string): void => {
-  console.log(arg)
+import { View, h } from './framework//view'
+import { ActionTree } from './framework/action'
+import { App } from './framework/controller'
+
+type State = typeof state;
+type Actions = typeof actions;
+
+const state = {
+  count: 0
 }
 
-log('test')
+const actions: ActionTree<State> = {
+  increment: (state: State) => {
+    state.count++
+  },
+  decrement: (state: State) => {
+    state.count--
+  }
+}
+
+const view: View<State, Actions> = (state, actions) => {
+  return h(
+    "div",
+    null,
+    h("p", null, state.count),
+    h("button", {type: "button", onclick: () => actions.increment(state)}, 'countUp'),
+    h("button", {type: "button", onclick: () => actions.decrement(state)}, 'countDown'),
+  )
+}
+
+new App<State, Actions>({
+  el: "#app",
+  state,
+  view,
+  actions,
+})
